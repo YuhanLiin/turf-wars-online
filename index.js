5da1a68859b10e49e6f28370bb5c8b61a5d7f897
Yuhan Lin <yl2017@dhltd.corp>
2017-03-28 16:33:26 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+var http = require("http").createServer();
+var io = require("socket.io")(http);
+var url = require("url");
+var fs = require("fs");
+
+var repo = require("./repository.js")
+
+function sendFile(res, filepath) {
+    fs.readFile('.'+filepath, function (err, data) {
+        if (err) {
+            res.statusCode = 404;
+            res.end("404: " + filepath + " not found");
+        }
+        else {
+            res.end(data);
+        }
+    })
+}
+
+http.on('request', function (req, res) {
+    var urlObj = url.parse(req.url, true);
+    var path = urlObj.path;
+    var query = urlObj.query;
+
+    res.setHeader("content-type", "text/html");
+    if (req.method === 'GET') {
+        if (path === '/') {
+            sendFile(res, "/views/index.html");
+        }
+    }
+    else if (req.method === 'POST') {
+        if (path === '/newRoom') {
+            sendFile(res, '/views/newRoom.html');
+        }
+        else if (path === '/lobby') {
+            repo.getRooms(function (err, rooms) {
+                if (err) console.log(err.stack);
+                //Templating will be required
+                else sendFile(res, '/views/lobby.html');
+            });
+        }
+    }
+}).listen(8000);
+
+io.of('newRoom').on('connect', function (socket) {
+    repo.addRoom(socket.id);
+
+    socket.on("disconnect", function () {
+        repo.deleteRoom(socket.id);
+    });
+});
+
+io.of('lobby').on('connect', function (socket) {
+    
+});
---
+++ repository.js
@@ -0,0 +1,56 @@
+// JavaScript source code
+var redis = require("redis");
+
+//Need to add config
+var pub = redis.createClient();
+var sub = redis.createClient();
+sub.subscribe('Rooms');
+
+//Might be changed later
+function toRoomId(socketId) {
+    return "room:" + socketId
+}
+
+function Room(roomId, hostId, guestId) {
+    return { id: roomId, host: hostId, guestId: guestId};
+}
+
+function addRoom(hostId, callback = (e, r) => { }) {
+    var roomId = toRoomId(hostId);
+    pub.multi().hset(roomId, 'host', hostId).sadd('availableRooms', roomId).exec(function (err, unused) {
+        var room = Room(roomId, hostId);
+        if (!err) pub.publish("Rooms", "add", room);
+        callback(err, room);
+    });
+}
+
+function deleteRoom(hostId, callback = (e, r) => { }) {
+    var roomId = toRoomId(hostId);
+    pub.multi().del(roomId).srem('availableRooms', roomId).exec(function (err, reply) {
+        if (!err) pub.publish('Rooms', 'delete', roomId);
+        callback(err, reply);
+    });
+}
+
+function getRooms(callback = (e, r) => { }){
+    pub.smembers('availableRooms', function(err, reply){
+        if (err) callback(err, reply);
+        else {
+            var rooms = [];
+            for (let i=0; i<reply.length; i++){
+                let roomId = reply[i];
+                pub.hgetall(roomId, function(err, data){
+                    if (err) console.log(err.stack);
+                    else rooms.push(data);
+                });
+            }
+            //Run callback with the list of rooms as a param
+            callback(err, rooms);
+        }
+    }
+}
+
+module.exports.pub = pub;
+module.exports.sub = sub;
+module.exports.addRoom = addRoom;
+module.exports.deleteRoom = deleteRoom;
