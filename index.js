d7582afb8576448698577190fd2caf9569179269
Yuhan Lin <yl2017@dhltd.corp>
2017-03-30 17:00:12 -0400

    Got redis to work.

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ function sendFile(res, filepath, context={}, type="text/html") {
             var data = data.toString();
             for (let prop in context){
                 if (context.hasOwnProperty(prop)){
-                    console.log(prop, `{{${prop}}}`, context[prop].toString());
                     data = data.replace(`{{${prop}}}`, context[prop].toString());
                 }
             }
@@ -41,11 +40,8 @@ http.on('request', function (req, res) {
             sendFile(res, '/views/newRoom.html');
         }
         else if (path === '/lobby') {
-            repo.getRooms(function (err, rooms) {
-                if (err) console.log(err.stack);
-                    //Templating will be required
-                else sendFile(res, '/views/lobby.html', {rooms: JSON.stringify(rooms)});
-            });
+            repo.getRooms().then(function(rooms){ sendFile(res, '/views/lobby.html', {rooms:JSON.stringify(rooms)}); },
+                               function(err){ console.log(err); } );
         }
     }
 
@@ -54,14 +50,26 @@ http.on('request', function (req, res) {
     });
 }).listen(8000);
 
+
 io.of('newRoom').on('connect', function (socket) {
-    repo.addRoom(socket.id);
+    repo.addRoom(socket.id)
+    .catch(function(err){ console.log(err); });
 
     socket.on("disconnect", function () {
-        repo.deleteRoom(socket.id);
+        repo.deleteRoom(socket.id)
+        .catch(function(err){ console.log(err); });
     });
 });
 
 io.of('lobby').on('connect', function (socket) {
-    
+    function pubsubHandler(channel, message){
+        console.log(channel);
+        socket.emit(channel.replace('Rooms/', ''), message);
+    }
+
+    repo.sub.on("message", pubsubHandler);
+
+    socket.on('disconnect', function (){
+        repo.sub.removeListener("message", pubsubHandler);
+    })
 });
--- repository.js
+++ repository.js
@@ -1,54 +1,67 @@
 // JavaScript source code
 var redis = require("redis");
 
+var models = require("./models.js");
+
 //Need to add config
-var url = process.env.REDIS_URL || "redis://h:p55843f5bc6952dfc491bba4494b013e5c0dd8b96f42ad6bf39156327a26a50a8@ec2-34-206-56-30.compute-1.amazonaws.com:16869";
+var url = process.env.REDIS_URL || "redis://127.0.0.1:6379";
 var pub = redis.createClient(url);
 var sub = redis.createClient(url);
-sub.subscribe('Rooms/*');
+sub.subscribe('Rooms/add', 'Rooms/delete');
 pub.flushdb();
 
 //Might be changed later
 function toRoomId(socketId) {
-    return "room:" + socketId
+    return socketId;
 }
 
-function Room(roomId, hostId, guestId) {
-    return { id: roomId, host: hostId, guestId: guestId};
-}
-
-function addRoom(hostId, callback = (e, r) => { }) {
-    var roomId = toRoomId(hostId);
-    pub.multi().hset(roomId, 'host', hostId).sadd('availableRooms', roomId).exec(function (err, unused) {
-        var room = Room(roomId, hostId);
-        if (!err) pub.publish("Rooms/add", JSON.stringify(room));
-        callback(err, room);
-    });
-}
-
-function deleteRoom(hostId, callback = (e, r) => { }) {
-    var roomId = toRoomId(hostId);
-    pub.multi().del(roomId).srem('availableRooms', roomId).exec(function (err, reply) {
-        if (!err) pub.publish('Rooms/delete', roomId);
-        callback(err, reply);
-    });
-}
-
-function getRooms(callback = (e, r) => { }){
-    pub.smembers('availableRooms', function(err, reply){
-        if (err) callback(err, reply);
-        else {
-            var rooms = [];
-            for (let i=0; i<reply.length; i++){
-                let roomId = reply[i];
-                pub.hgetall(roomId, function(err, data){
-                    if (err) console.log(err.stack);
-                    else rooms.push(data);
-                });
+function addRoom(hostId) {
+    return new Promise(function(resolve, reject){
+        var roomId = toRoomId(hostId);
+        var room = models.Room(roomId, hostId);
+        pub.multi().hmset("Room:"+roomId, room).sadd('availableRooms', roomId).exec(function (err, unused) {
+            if (err) reject(err);
+            else{
+                pub.publish("Rooms/add", JSON.stringify(room));
+                resolve();
             }
-            //Run callback with the list of rooms as a param
-            callback(err, rooms);
-        }
+        });
+    });
+}
+
+function deleteRoom(hostId) {
+    return new Promise(function(resolve, reject){
+        var roomId = toRoomId(hostId);
+        pub.multi().del("Room:" + roomId).srem('availableRooms', roomId).exec(function (err, reply) {
+            if (err) reject(err);
+            else{
+                pub.publish('Rooms/delete', roomId);
+                resolve();
+            }
+        });
+    });
+}
+
+function getRooms() {
+    return new Promise(function (resolve, reject) {
+        pub.smembers('availableRooms', function (err, roomIds) {
+            if (err) reject(err);
+            else {
+                resolve(roomIds);
+            }
+        });
+    })
+    .then(function (roomIds) {
+        return Promise.all(roomIds.map(function (id) {
+            return new Promise(function (resolve, reject) {
+                pub.hgetall("Room:" + id, function (err, data) {
+                    if (err) reject(err);
+                    else {
+                        resolve(data);
+                    }
+                });
+            });
+        }));
     });
 }
 
