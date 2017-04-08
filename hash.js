19cea43fe5713530e76775f2453bb64e70b5ff66
yuhan lin <yuhan lin>
2017-04-08 02:35:56 -0400

    Removed unecessary files and packages

--- hash.js
+++
@@ -1,9 +0,0 @@
-var shorthash = require('shorthash');
-
-var secret = 'lol';
-
-function sh(string){
-    return shorthash.unique(string+secret);
-}
-
-module.exports.sh = sh;
--- models.js
+++
@@ -1,7 +0,0 @@
-function Room(roomId, hostId) {
-    return Object.assign(Object.create(Room.prototype), 
-        { id: roomId, host: hostId });
-}
-Room.prototype = {};
-
-module.exports.Room = Room;
